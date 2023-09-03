4dda825c0acb51d71c4663e18a2286db7bcdd987
mcr45 <depr3604@protonmail.com>
2023-09-03 18:38:15 -0500

    script for adding books&button for removal

--- main.js
+++ main.js
@@ -41,22 +41,27 @@ function loadBook(){
 let tab=document.querySelector('#tab')
 
 
-/* td.innerText='hey'
-tr.appendChild(td)
-tab.appendChild(tr) */
 
-myLibr.forEach((item)=>{
+
+myLibr.forEach((item,index)=>{
     let tr=document.createElement('tr')
 let tdtitle=document.createElement('td')
 let tdauthor =document.createElement('td')
 let tdpages=document.createElement('td')
 let tdfinish=document.createElement('td')
+let tbut=document.createElement('td')
 tdtitle.innerText=item.title
+tdtitle.setAttribute("data-attribute",index)
 tdauthor.innerHTML=item.author
+tdauthor.setAttribute("data-attribute",index)
 tdpages.innerText=item.pages
+tdpages.setAttribute("data-attribute",index)
 tdfinish.innerText=item.finish ? 'read': 'not read yet'
-
-tr.append(tdtitle,tdauthor,tdpages,tdfinish)
+tdfinish.setAttribute("data-attribute",index)
+tbut.innerHTML=`<button data-attribute='${index}'>update</button>`
+tbut.setAttribute("data-attribute",index)
+tbut.classList.add('update')
+tr.append(tdtitle,tdauthor,tdpages,tdfinish,tbut)
 tab.appendChild(tr)
 })
 
@@ -64,7 +69,64 @@ tab.appendChild(tr)
 }
 
 loadBook()
+/* 
+prova send books on click */
+
+const mydemo=[/* {
+  title:'Vuoto ap e',
+  author:'John',
+  pages:77,
+  finish:false
+} */]
+
+function sendBook(e){
+  e.preventDefault()
+  let tab=document.querySelector('#tab')
+let form=document.querySelector('#prova')
+let title=document.querySelector("#title")
+let author=document.querySelector('#author')
+let pages=document.querySelector('#pages')
+/* console.log(title.value) */
+let book={title:title.value,
+author:author.value,
+pages:pages.value,
+finish:false
+}
+
+mydemo.push(book)
+
+form.style.display='none'
+
+  mydemo.forEach((item)=>{
+      let tr=document.createElement('tr')
+  let tdtitle=document.createElement('td')
+  let tdauthor =document.createElement('td')
+  let tdpages=document.createElement('td')
+  let tdfinish=document.createElement('td')
+  tdtitle.innerText=item.title
+  tdauthor.innerHTML=item.author
+  tdpages.innerText=item.pages
+  tdfinish.innerText=item.finish ? 'read': 'not read yet'
+  
+  tr.append(tdtitle,tdauthor,tdpages,tdfinish)
+  tab.appendChild(tr)
+  
+  })
+
+}
+let but=document.querySelector('#send')
+but.addEventListener('click',sendBook)
+
+let updatebutton=document.querySelectorAll('.update')
+let buttons=Array.from(updatebutton)
+buttons.forEach((item)=>{
+  item.addEventListener('click', removeBook)
+})
+/* updatebutton */
+
+function removeBook(e){
+let y=e.target.getAttribute("data-attribute")
+console.log(y)
 
 
-
-
+}
